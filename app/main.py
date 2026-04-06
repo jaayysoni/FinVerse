43d8781a86337487d9fa0a300c495357c2670705
JayySoni <sonijaysoni459@gmail.com>
2026-04-06 19:05:08 +0530

    Final Project is Complete

--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
 # app/main.py
 from fastapi import FastAPI
 from starlette.middleware.sessions import SessionMiddleware
+from fastapi.templating import Jinja2Templates
 from app.api.routes import users
+from pathlib import Path
 
 app = FastAPI()
 
+# Session middleware
 app.add_middleware(SessionMiddleware, secret_key="super-secret-key")
 
+# Include routes
 app.include_router(users.router)
+
+# Configure templates
+BASE_DIR = Path(__file__).parent
+templates = Jinja2Templates(directory=BASE_DIR / "templates")
